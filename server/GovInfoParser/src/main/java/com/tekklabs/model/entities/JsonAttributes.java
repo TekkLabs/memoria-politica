77a72aa2da3cb88fb6a14561fde745cf8a9f65e4
TACIO SILVA DIOGO                                                     C0000000 <y5kx@mi00245774.ep.petrobras.com.br>
2016-04-29 20:13:58 -0300

    Criação de um utilitário para extrair imagens JPEG do pdf Carometro da câmara dos deputados. Esse utilitário também consegue processar imagens JPEG nos modelos de cores RGB e CMYK para aplicar compressão em 30%.

---
+++ server/GovInfoParser/src/main/java/com/tekklabs/model/entities/JsonAttributes.java
@@ -0,0 +1,17 @@
+package com.tekklabs.model.entities;
+
+/**
+ * Created by PC on 22/07/2015.
+ */
+public class JsonAttributes {
+    public static final String NOME_CIVIL = "nome_civil";
+    public static final String NOME_POLITICO = "nome_politico";
+    public static final String CPF_CANDIDATO = "cpf";
+    public static final String NUM_TITULO_ELEITORAL_CANDIDATO = "num_titulo_eleitoral";
+    public static final String SIGLA_UF = "uf";
+    public static final String PARTIDO = "partido";
+    public static final String STATUS = "status"; // Titular/Suplente/Efetivado / Candidato
+    public static final String EMAIL = "email";
+    public static final String PHONE = "telefone";
+    public static final String FAX = "fax";
+}
